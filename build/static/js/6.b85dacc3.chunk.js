627f3b6f59629c52ce24395e969b776aed15a801
adebammytayo123 <bamianike@gmail.com>
2021-06-24 15:35:51 +0100

    run build

---
+++ build/static/js/6.b85dacc3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[6],{761:function(a,s,d){}}]);
+//# sourceMappingURL=6.b85dacc3.chunk.js.map
