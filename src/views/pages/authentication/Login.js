8382289bc1694445f6d21473d60d3d6786fbf666
Emmanuel Adelugba <adelugba.emma@gmail.com>
2021-07-13 09:33:12 +0000

    ...

--- src/views/pages/authentication/Login.js
+++ src/views/pages/authentication/Login.js
@@ -42,7 +42,7 @@ const ToastContentValid = ({ name, role }) => (
 )
 
 
-const ToastContentInvalid = ({ message }) => (
+const InvalidLoginToastContent = ({ message }) => (
   <Fragment>
     <div className='toastify-header'>
       <div className='title-wrapper'>
@@ -51,7 +51,7 @@ const ToastContentInvalid = ({ message }) => (
       </div>
     </div>
     <div className='toastify-body'>
-      <span>invalid</span>
+      <span>Pls, reconfirm login credentials</span>
     </div>
   </Fragment>
 )
@@ -62,21 +62,24 @@ const Login = props => {
   const ability = useContext(AbilityContext)
   const dispatch = useDispatch()
   const history = useHistory()
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('tosinolugbenga@gmail.com')
+  const [password, setPassword] = useState('kfxHdSCqM')
   const [isSubmitting, setIsSubmitting] = useState(false)
 
   const illustration = skin === 'dark' ? 'login-v2-dark.svg' : 'login-v2.svg',
     source = require(`@src/assets/images/pages/${illustration}`).default
 
   const handleSubmit = async (event, errors) => {
+    console.log(event, errors)
       if (errors && !errors.length) {
         setIsSubmitting(true)
+        // const isLogin = await useJwt.login({ email, password })
+        // console.log('hi', isLogin)
         await useJwt
           .login({ email, password })
           .then(res => {
-            console.log("ress", res)
-            if (res?.data.success) {
+            console.log({res})
+            if (res.data.success) {
               const data = {
                 ...res.data.data,
                 accessToken: res.data.token,
@@ -100,7 +103,7 @@ const Login = props => {
               setIsSubmitting(false)
             }
           })
-          .catch(err => console.log(err))
+          .catch(err => { console.log(err); setIsSubmitting(false) })
       }
   }
 
